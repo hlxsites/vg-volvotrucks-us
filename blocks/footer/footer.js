4b0d90d939bcaa1e82a65ede8b527e1c4439ef98
Burkhard Pauli <mail@bpauli.de>
2023-02-17 16:26:27 +0100

    add copyright handling to footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig, decorateIcons } from "../../scripts/lib-franklin.js";
 
 /**
  * loads and decorates the footer
@@ -8,12 +8,17 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
-
+  const currentYear = new Date().getFullYear();
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
-  const footer = document.createElement('div');
-  footer.innerHTML = html;
+  const footer = document.createElement("div");
+  footer.innerHTML = html.replaceAll('{year}', currentYear);
+  const footerItems = [...footer.children];
+  if (footerItems.length >= 2) {
+    footerItems[0].classList.add('footer--links');
+    footerItems[1].classList.add('footer--copyright');
+  }
   await decorateIcons(footer);
   block.append(footer);
 }
