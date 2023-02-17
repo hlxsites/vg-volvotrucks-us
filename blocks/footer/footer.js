c6228ffac089432edf7ad5dcefb766ecc1a3e7dd
Burkhard Pauli <mail@bpauli.de>
2023-02-17 16:36:21 +0100

    fix linting issue

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from "../../scripts/lib-franklin.js";
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
@@ -12,7 +12,7 @@ export default async function decorate(block) {
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
-  const footer = document.createElement("div");
+  const footer = document.createElement('div');
   footer.innerHTML = html.replaceAll('{year}', currentYear);
   const footerItems = [...footer.children];
   if (footerItems.length >= 2) {
