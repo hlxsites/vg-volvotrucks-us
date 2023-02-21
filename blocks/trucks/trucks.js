35eefbda1e8e04cf4bc58f3bb9f7d767b7237f4e
Burkhard Pauli <mail@bpauli.de>
2023-02-21 16:13:24 +0100

    Trucks overview

--- blocks/trucks/trucks.js
+++ blocks/trucks/trucks.js
@@ -1,29 +1,71 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
-  block.querySelectorAll('ul > li').forEach((li) => {
+function updateSlide(index, $block) {
+  const $trucksContainer = $block.querySelector('.trucks-list');
+  $block.querySelector('.controls li.active').classList.remove('active');
+  $block.querySelectorAll('.controls li')[index].classList.add('active');
 
+  $trucksContainer.style.transform = `translateX(-${index * 380 + 80}px)`;
+}
+
+export default function decorate($block) {
+  const $gridContainer = $block.querySelector('ul');
+  $gridContainer.classList.add("trucks-list");
+  $block.append($gridContainer);
+  
+  const $truckItems = $block.querySelectorAll('ul > li');
+  $truckItems.forEach(($li) => {
     // Add wrapper around the content
-    const contentContainer = document.createElement('div');
-    contentContainer.classList.add('wrapper');
-    contentContainer.innerHTML = li.innerHTML;
-    li.innerHTML = '';
-    li.append(contentContainer);
+    const $contentContainer = document.createElement('div');
+    $contentContainer.classList.add('wrapper');
+    $contentContainer.innerHTML = $li.innerHTML;
+    $li.innerHTML = '';
+    $li.append($contentContainer);
 
     // add link to the image and move it outside of the wrapper
-    const link = li.querySelector('a');
-    const imageLink = link.cloneNode(false);
-    li.prepend(imageLink);
+    const $link = $li.querySelector('a');
+    const $imageLink = $link.cloneNode(false);
+    $li.prepend($imageLink);
 
-    const image = li.querySelector('picture');
-    imageLink.append(image);
+    const $image = $li.querySelector('picture');
+    $imageLink.append($image);
+    $imageLink.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '370' }])));
 
-    const textItems = contentContainer.innerHTML
+    const $textItems = $contentContainer.innerHTML
       .split('<br>').filter((text) => text.trim() !== '');
 
-    contentContainer.innerHTML = `
-      <h3>${textItems[0]}</h3>
-      <p>${textItems.slice(1).join('</p><p>')}</p>
+    $contentContainer.innerHTML = `
+      <p>${$textItems.join('</p><p>')}</p>
     `;
   });
+
+  // create carousel controls for mobile
+  const $controlsContainer = document.createElement('ul')
+  $controlsContainer.classList.add('controls')
+  $block.append($controlsContainer);
+  $truckItems.forEach((item, j) => {
+    const $controlItem = document.createElement('li');
+    $controlItem.innerHTML = `
+      <button type="button">` + (j + 1) + `</button>
+    `;
+    $controlsContainer.append($controlItem);
+  });
+
+  $block.querySelector('ul.controls li:first-child').classList.add('active');
+  const $controls = $block.querySelectorAll('ul.controls > li > button');
+  $controls.forEach(($control, j) => {
+    $control.addEventListener('click', () => {
+      updateSlide(j, $block);
+    });
+  })
+
+  // clone first and last truck item for carousel
+  const $firstItem = $block.querySelector('ul.trucks-list li:first-child');
+  const $lastItem = $block.querySelector('ul.trucks-list li:last-child');
+  const $firstItemClone = $firstItem.cloneNode(true);
+  const $lastItemClone = $lastItem.cloneNode(true);
+  $firstItemClone.classList.add('clone');
+  $lastItemClone.classList.add('clone');
+  $lastItem.after($firstItemClone);
+  $firstItem.before($lastItemClone);
 }
