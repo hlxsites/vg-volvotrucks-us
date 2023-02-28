cb059ace5d70850e2db16d9aeba6854ff0df6b43
Aravindo Wingeier <wingeier@adobe.com>
2023-02-28 14:41:25 -0800

    eslint . --fix

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,31 +2,29 @@
 import { sampleRUM } from './lib-franklin.js';
 
 const loadScript = (url, attrs) => {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.src = url;
-    if (attrs) {
-        // eslint-disable-next-line no-restricted-syntax, guard-for-in
-        for (const attr in attrs) {
-            script.setAttribute(attr, attrs[attr]);
-        }
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
     }
-    head.append(script);
-    return script;
+  }
+  head.append(script);
+  return script;
 };
-
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
 
-
 // OneTrust Cookies Consent Notice start
 loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
-    type: 'text/javascript',
-    charset: 'UTF-8',
-    'data-domain-script': 'ec7e1b30-6b41-4e0f-ae45-623aa2563922',
+  type: 'text/javascript',
+  charset: 'UTF-8',
+  'data-domain-script': 'ec7e1b30-6b41-4e0f-ae45-623aa2563922',
 });
 
 window.OptanonWrapper = () => {};
