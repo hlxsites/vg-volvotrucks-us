a2a6cfd74ee6458d58d0996fae06ce449fb9c86b
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-03-03 16:05:23 +0100

    #22 buttons has an icon & primary are white

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -495,7 +495,7 @@ export function decorateButtons(element) {
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
         if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button primary'; // default
+          a.className = 'button'; // default
           up.classList.add('button-container');
         }
         if (up.childNodes.length === 1 && up.tagName === 'STRONG'
