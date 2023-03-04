57fc50c75985c3745ddaa5ad96a9cad972ba318c
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-03-04 12:29:33 +0100

    #21 add-remove on resize and get parent fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,11 +19,26 @@ const LCP_BLOCKS = ['teaser-grid']; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
 function isCTALinkCheck(ctaLink) {
-  const btnContainer = ctaLink.parentElement;
+  const btnContainer = ['strong', 'em'].includes(ctaLink.parentElement.localName)
+    ? ctaLink.parentElement.parentElement
+    : ctaLink.parentElement;
   if (!btnContainer.classList.contains('button-container')) return false;
   const previousSibiling = btnContainer.previousElementSibling;
   const twoPreviousSibiling = previousSibiling.previousElementSibling;
   return previousSibiling.localName === 'h1' || twoPreviousSibiling.localName === 'h1';
+}
+
+function resizeButtonListener(btn) {
+  const MQ = window.matchMedia('(min-width: 992px)');
+  const primary = 'primary';
+  window.onresize = () => {
+    const hasPrimary = btn.classList.contains(primary);
+    if (MQ.matches && !hasPrimary) {
+      btn.classList.add(primary);
+    } else if (!MQ.matches && hasPrimary) {
+      btn.classList.remove(primary);
+    }
+  };
 }
 
 function buildHeroBlock(main) {
@@ -45,7 +60,10 @@ function buildHeroBlock(main) {
       headings.appendChild(h4);
     }
     headings.appendChild(h1);
-    if (ctaLink && isCTALink) headings.appendChild(ctaLink.parentElement);
+    if (ctaLink && isCTALink) {
+      headings.appendChild(ctaLink.parentElement);
+      resizeButtonListener(ctaLink);
+    }
     const section = document.createElement('div');
     section.append(buildBlock('hero', { elems }));
     // remove the empty pre-section to avoid decorate it as empty section
