d5e0af826637c8dcf74db60d663630f14157e50a
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-03-06 17:39:36 +0100

    #57 the loading process not block other loadings

--- blocks/press-release-disclaimer/press-release-disclaimer.js
+++ blocks/press-release-disclaimer/press-release-disclaimer.js
@@ -1,19 +1,18 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
-export default async function decorate(block) {
+export default function decorate(block) {
   const config = readBlockConfig(block);
-  block.textContent = '';
+  const disclaimerPath = config.disclaimer || '/news-and-stories/press-releases';
 
-  const { disclaimer } = config;
-  const disclaimerPath = disclaimer || '/news-and-stories/press-releases';
-  const resp = await fetch(`${disclaimerPath}/disclaimer.plain.html`);
+  fetch(`${disclaimerPath}/disclaimer.plain.html`)
+    .then((resp) => resp.text())
+    .then((html) => {
+      const container = document.createElement('div');
+      container.className = 'disclaimer-container';
+      container.innerHTML = html;
+      container.children[0].className = 'disclaimer-wrapper';
 
-  if (!resp.ok) return;
-  const html = await resp.text();
-  const container = document.createElement('div');
-  container.className = 'disclaimer-container';
-  container.innerHTML = html;
-  container.children[0].className = 'disclaimer-wrapper';
-
-  block.appendChild(container);
+      block.textContent = '';
+      block.appendChild(container);
+    });
 }
