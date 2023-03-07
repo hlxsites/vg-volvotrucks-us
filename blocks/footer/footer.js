b1aa904c557121393f28a4294ee69680dbb989f7
Burkhard Pauli <mail@bpauli.de>
2023-03-07 08:47:41 +0100

    fix: link target in footer and missing scroll to top

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,10 +1,37 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function scrollTopFunction(buttonEl) {
+  if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+    buttonEl.style.display = 'block';
+  } else {
+    buttonEl.style.display = 'none';
+  }
+}
+function goToTopFunction() {
+  let timeOut;
+  if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+    window.scrollBy(0, -50);
+    timeOut = setTimeout(goToTopFunction, 10);
+  } else {
+    clearTimeout(timeOut);
+  }
+}
+
+function displayScrollToTop(mainEl) {
+  const scrollToTopButton = document.createElement('button');
+  scrollToTopButton.addEventListener('click', goToTopFunction);
+  window.addEventListener('scroll', () => scrollTopFunction(scrollToTopButton));
+  scrollToTopButton.classList.add('scroll-to-top');
+  scrollToTopButton.setAttribute('title', 'Go to the top of the page');
+  scrollToTopButton.innerHTML = `
+    <i class="fa fa-angle-up"><i/>
+  `;
+  mainEl.append(scrollToTopButton);
+}
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
@@ -14,6 +41,18 @@ export default async function decorate(block) {
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html.replaceAll('{year}', currentYear);
+  const anchors = footer.querySelectorAll('a');
+  anchors.forEach((anchor) => {
+    try {
+      const { origin } = new URL(anchor.href, window.location.href);
+      if (origin && origin !== window.location.origin) {
+        anchor.setAttribute('target', '_blank');
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn(`Invalid link ${anchor.href}`);
+    }
+  });
   const footerItems = [...footer.children];
   if (footerItems.length >= 2) {
     footerItems[0].classList.add('footer-links');
@@ -21,4 +60,5 @@ export default async function decorate(block) {
   }
   await decorateIcons(footer);
   block.append(footer);
+  displayScrollToTop(block);
 }
