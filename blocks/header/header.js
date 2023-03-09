fb75b44cd37866ed370ebfc2f27efbedfe64114b
msagolj <msagolj@adobe.com>
2023-03-09 18:18:19 +0100

    removed commented code, use new nav.doc structure

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -3,90 +3,6 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 // media query match that indicates mobile/tablet width
 const MQ = window.matchMedia('(min-width: 992px)');
 
-/* function closeOnEscape(e) {
-  if (e.code === 'Escape') {
-    const nav = document.getElementById('nav');
-    const navSections = nav.querySelector('.nav-sections');
-    const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
-    if (navSectionExpanded && MQ.matches) {
-      // eslint-disable-next-line no-use-before-define
-      toggleAllNavSections(navSections);
-      navSectionExpanded.focus();
-    } else if (!MQ.matches) {
-      // eslint-disable-next-line no-use-before-define
-      toggleMenu(nav, navSections);
-      nav.querySelector('button').focus();
-    }
-  }
-}
-
-function openOnKeydown(e) {
-  const focused = document.activeElement;
-  const isNavDrop = focused.className === 'nav-drop';
-  if (isNavDrop && (e.code === 'Enter' || e.code === 'Space')) {
-    const dropExpanded = focused.getAttribute('aria-expanded') === 'true';
-    // eslint-disable-next-line no-use-before-define
-    toggleAllNavSections(focused.closest('.nav-sections'));
-    focused.setAttribute('aria-expanded', dropExpanded ? 'false' : 'true');
-  }
-}
-
-function focusNavSection() {
-  document.activeElement.addEventListener('keydown', openOnKeydown);
-}
-
-/**
- * Toggles all nav sections
- * @param {Element} sections The container element
- * @param {Boolean} expanded Whether the element should be expanded or collapsed
- *
-function toggleAllNavSections(sections, expanded = false) {
-  sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
-    section.setAttribute('aria-expanded', expanded);
-  });
-}
-
-/**
- * Toggles the entire nav
- * @param {Element} nav The container element
- * @param {Element} navSections The nav sections within the container element
- * @param {*} forceExpanded Optional param to force nav expand behavior when not null
- *
-function toggleMenu(nav, navSections, forceExpanded = null) {
-  const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded')
-   === 'true';
-  const button = nav.querySelector('.nav-hamburger button');
-  document.body.style.overflowY = (expanded || MQ.matches) ? '' : 'hidden';
-  nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(navSections, expanded || MQ.matches ? 'false' : 'true');
-  button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
-  // enable nav dropdown keyboard accessibility
-  const navDrops = navSections.querySelectorAll('.nav-drop');
-  if (MQ.matches) {
-    navDrops.forEach((drop) => {
-      if (!drop.hasAttribute('tabindex')) {
-        drop.setAttribute('role', 'button');
-        drop.setAttribute('tabindex', 0);
-        drop.addEventListener('focus', focusNavSection);
-      }
-    });
-  } else {
-    navDrops.forEach((drop) => {
-      drop.removeAttribute('role');
-      drop.removeAttribute('tabindex');
-      drop.removeEventListener('focus', focusNavSection);
-    });
-  }
-  // enable menu collapse on escape keypress
-  if (!expanded || MQ.matches) {
-    // collapse menu on escape press
-    window.addEventListener('keydown', closeOnEscape);
-  } else {
-    window.removeEventListener('keydown', closeOnEscape);
-  }
-}
-*/
-
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -147,9 +63,16 @@ export default async function decorate(block) {
       </div>
 
       <div class='sections'>
+        <div class='sections-list'>
+        </div>
       </div>
 
       <div class='semitrans'>
+      </div>
+
+      <div class='hamburger-menu'>
+        <div class='sections-list'>
+        </div>
       </div>
     `;
 
@@ -162,11 +85,36 @@ export default async function decorate(block) {
     nav.querySelector('.location').append(navContent.querySelector('div:first-of-type > p:nth-of-type(3)').textContent);
     // tools
     nav.querySelector('.tools').prepend(navContent.querySelector('div:nth-of-type(2) ul'));
-    // sections
-    nav.querySelector('.sections').append(navContent.querySelector('div:nth-of-type(3) ul'));
+
+    // get through all section menus
+    let sectionMenu = '';
+    let hamburgerMenu = '';
+
+    const sections = navContent.querySelectorAll('.menu');
+    if (sections) {
+      sections.forEach((section) => {
+        // go through each entry in the menu table
+        [...section.children].forEach((entry) => {
+          switch (entry.children[0].textContent) {
+            // name of the section
+            case 'Name':
+              // add to section menu
+              sectionMenu += `<div class='section'><a href='#'>${entry.children[1].textContent}</a></div>`;
+              // add to hamburger menu
+              hamburgerMenu += `<div class='section'><a href='#'>${entry.children[1].textContent}</a></div>`;
+              break;
+            default:
+              break;
+          }
+        });
+      });
+      // write the section menu
+      nav.querySelector('.sections .sections-list').innerHTML = sectionMenu;
+      // write the hamburger menu
+      nav.querySelector('.hamburger-menu .sections-list').innerHTML = hamburgerMenu;
+    }
 
     // add event listeners
-
     // for the mobile search icon
     nav.querySelector('.search-toggle').addEventListener('click', (e) => {
       const expanded = e.currentTarget.getAttribute('aria-expanded') === 'true';
@@ -189,21 +137,6 @@ export default async function decorate(block) {
     MQ.addEventListener('change', () => {
       document.querySelector('nav .hamburger-toggle').setAttribute('aria-expanded', 'false');
     });
-
-    /* const navSections = nav.querySelector('.nav-sections');
-    if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-        navSection.addEventListener('click', () => {
-          if (MQ.matches) {
-            const expanded = navSection.getAttribute('aria-expanded') === 'true';
-            toggleAllNavSections(navSections);
-            navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-          }
-        });
-      });
-    }
-    */
 
     /* set volvo icon */
     decorateIcons(nav);
