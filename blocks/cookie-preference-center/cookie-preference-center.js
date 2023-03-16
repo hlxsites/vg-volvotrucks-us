fd2fce571ab5e2946fd9cbe9426954b2a1bfcd48
Aravindo Wingeier <synox@users.noreply.github.com>
2023-03-16 03:49:00 -0700

    feat: cookie styles (#109)
    * Improve margin and font-sizes for cookie list
    
    * Add favicon
    
    * Fix "Cookie preference center" text
    
    * Style "Cookie preference center" button
    
    * Add basic text block
    
    * remove [data-align="center"] and replace with text block
    
    * fix: link style on teaser cards
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Darin Kuntze <dkuntze@gmail.com>
    Co-authored-by: Dirk Rudolph <drudolph@adobe.com>

--- blocks/cookie-preference-center/cookie-preference-center.js
+++ blocks/cookie-preference-center/cookie-preference-center.js
@@ -1,15 +1,9 @@
 export default function decorate(block) {
-  const buttonContainer = document.createElement('p');
-  buttonContainer.classList.add('button-container');
-
-  const button = document.createElement('button');
-  button.classList.add('button');
-  button.classList.add('secondary');
-  button.classList.add('ot-sdk-show-settings');
-
+  const button = document.createElement('a');
+  button.classList.add('cta');
+  button.setAttribute('onclick', 'OneTrust.ToggleInfoDisplay();');
   button.textContent = block.textContent;
-  buttonContainer.append(button);
 
   block.textContent = '';
-  block.append(buttonContainer);
+  block.append(button);
 }
