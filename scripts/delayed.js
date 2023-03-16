db65f797000e410e7b06df1e1bcb629989769f7c
Aravindo Wingeier <wingeier@adobe.com>
2023-03-16 09:38:06 -0700

    on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens, because the cookie is not persistent. To avoid this annoyance, disable unless on the production page.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,12 +6,17 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-// OneTrust Cookies Consent Notice start
-loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
-  type: 'text/javascript',
-  charset: 'UTF-8',
-  'data-domain-script': 'ec7e1b30-6b41-4e0f-ae45-623aa2563922',
-});
+// OneTrust Cookies Consent Notice
+if (window.location.host.includes('volvotrucks.us')) {
+  // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
+  // because the cookie is not persistent. To avoid this annoyance, disable unless on the
+  // production page.
+  loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
+    type: 'text/javascript',
+    charset: 'UTF-8',
+    'data-domain-script': 'ec7e1b30-6b41-4e0f-ae45-623aa2563922',
+  });
 
-window.OptanonWrapper = () => {};
-// OneTrust Cookies Consent Notice end
+  window.OptanonWrapper = () => {
+  };
+}
