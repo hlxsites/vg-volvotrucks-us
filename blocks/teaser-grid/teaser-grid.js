005c7eed8883446c1776683ce98dfd3e57b1b517
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2023-03-27 15:55:15 +0200

    #111 Add top-ten style version to teaser grid component

--- blocks/teaser-grid/teaser-grid.js
+++ blocks/teaser-grid/teaser-grid.js
@@ -1,6 +1,7 @@
 import { addVideoShowHandler, isVideoLink, wrapImageWithVideoLink } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+  const isTopTenVersion = block.classList.contains('top-ten');
   const grid = document.createElement('ul');
   const columns = [...block.firstElementChild.children];
   const colCount = columns.length;
@@ -10,14 +11,12 @@ export default function decorate(block) {
     return !!li.parentElement;
   }).length).reduce((l, r) => Math.max(l, r), 0);
 
-  grid.style.setProperty('--grid-row-count', rowCount);
-  grid.style.setProperty('--grid-col-count', colCount);
-
   columns.forEach((column) => {
     const lis = column.querySelectorAll(':scope > ul > li');
     const liCount = lis.length;
     lis.forEach((li, i) => {
       const contentContainer = document.createElement('div');
+      contentContainer.classList.add('teaser-grid-text-wrapper');
       contentContainer.innerHTML = li.innerHTML;
       li.innerHTML = '';
 
@@ -34,11 +33,27 @@ export default function decorate(block) {
       li.appendChild(link ?? image);
 
       const textItems = contentContainer.innerHTML.split('<br>').filter((text) => text.trim() !== '');
-      if (textItems.length) {
+      if (textItems.length && !isTopTenVersion) {
         let html = `<h3>${textItems[0]}</h3>`;
         if (textItems.length > 1) {
           html += `<p>${textItems.slice(1).join('</p><p>')}</p>`;
         }
+        contentContainer.innerHTML = html;
+        li.appendChild(contentContainer);
+      }
+      if (isTopTenVersion && textItems.length) {
+        const isNumberHeader = !Number.isNaN(parseInt(textItems[0], 10));
+        const heading = isNumberHeader ? textItems[0] : '';
+        const subheading = textItems[isNumberHeader ? 1 : 0];
+        const description = textItems.slice(isNumberHeader ? 2 : 1).join('</p><p>');
+        let html = '';
+
+        if (heading) {
+          html += `<h3>${heading}</h3>`;
+        }
+
+        html += `<h4>${subheading}</h4>`;
+        html += `<p>${description}</p>`;
         contentContainer.innerHTML = html;
         li.appendChild(contentContainer);
       }
@@ -51,6 +66,8 @@ export default function decorate(block) {
   });
 
   block.innerHTML = grid.outerHTML;
+  block.style.setProperty('--grid-row-count', rowCount);
+  block.style.setProperty('--grid-col-count', colCount);
 
   // iterating through the list after the HTML manipulation
   const links = [...block.querySelectorAll('a')];
@@ -59,4 +76,25 @@ export default function decorate(block) {
     addVideoShowHandler(link);
     wrapImageWithVideoLink(link, link.querySelector('picture'));
   });
+
+  // checking if element is visible on the screen to start animation for top-ten version
+  if (block.classList.contains('top-ten')) {
+    const options = {
+      threshold: [0.25, 0.70],
+    };
+    const items = block.querySelectorAll('li');
+
+    [...items].forEach((item) => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const action = entry.intersectionRatio < 0.7 ? 'remove' : 'add';
+
+            item.classList[action]('top-ten-active');
+          }
+        });
+      }, options);
+      observer.observe(item);
+    });
+  }
 }
