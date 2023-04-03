fe29cae5cc75db08ed7763d95c3bcd9dd3ed67b4
Darin Kuntze <dkuntze@gmail.com>
2023-04-03 16:01:14 -0500

    fix multi-line

--- blocks/document-list/document-list.js
+++ blocks/document-list/document-list.js
@@ -1,11 +1,12 @@
 export default async function decorate(block) {
-  const buttons = block.querySelectorAll('div > div.button-container > a');
+  const buttons = block.querySelectorAll('a');
   buttons.forEach((button) => {
-    button.classList.remove('button');
+    button.classList.remove(['button']);
   });
   const lists = block.querySelectorAll('div > div > ul');
   lists.forEach((list) => {
     const contDiv = document.createElement('div');
+    contDiv.classList.add('multiline');
     list.querySelectorAll('li').forEach((item) => {
       const itm = document.createElement('div');
       itm.innerHTML = item.innerHTML;
