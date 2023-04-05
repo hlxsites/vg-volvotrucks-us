ea08c97a76aa0fff3827d9850f8ef568374d66a9
Aravindo Wingeier <wingeier@adobe.com>
2023-04-04 17:16:18 -0700

    use query-index.json for download-content-markdown.mjs

--- tools/download-content-markdown.mjs
+++ tools/download-content-markdown.mjs
@@ -6,6 +6,8 @@ import fetch from 'node-fetch';
 import fs from "fs";
 import path from "path";
 
+const DOMAIN = "https://main--vg-volvotrucks-us--hlxsites.hlx.page";
+
 async function createFolder(url) {
     const folderPath = `./mirror${new URL(url).pathname.replace('/.*$', '')}`;
     try {
@@ -23,25 +25,36 @@ function mdUrl(url) {
     }
 }
 
+
 async function getUrls() {
-    const response = await fetch("https://main--vg-volvotrucks-us--hlxsites.hlx.page/drafts/import-status.json");
+    const response = await fetch(DOMAIN + "/query-index.json");
     const json = await response.json();
-    return json.data.filter(row => !row.Status.includes("removed"))
-        .map(row => row.Preview);
+    return json.data.map(row => DOMAIN + row.path + "/")
+        // remove duplicate slashes (usually at the end)
+        .map(url => url.replaceAll("//", "/"));
+}
+
+async function loadUrl(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const folderPath = await createFolder(url);
+        await response.body.pipe(fs.createWriteStream(`${folderPath}/${path.basename(url)}`));
+        console.log('ok: ', url);
+    } else {
+        if(url.includes("/index.md") && response.status === 404){
+            // retry without "/index"
+            return loadUrl(url.replace("/index.md", ".md"));
+        } else {
+            console.log('failed: ', url);
+        }
+    }
 }
 
 async function main() {
     const urls = await getUrls()
     await Promise.all(urls.map(async (url) => {
         const adjustedUrl = mdUrl(url);
-        const response = await fetch(adjustedUrl);
-        if (response.ok) {
-            const folderPath = await createFolder(adjustedUrl);
-            await response.body.pipe(fs.createWriteStream(`${folderPath}/${path.basename(adjustedUrl)}`));
-            console.log('ok: ', adjustedUrl);
-        } else {
-            console.log('failed: ', adjustedUrl);
-        }
+        await loadUrl(adjustedUrl);
     }));
 }
 
