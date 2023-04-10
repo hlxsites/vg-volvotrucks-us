c688912063fc02695decd63c56898a61e74a89eb
Aravindo Wingeier <wingeier@adobe.com>
2023-04-10 16:41:18 -0700

    make finding a hero more flexible

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,6 +43,12 @@ function isCTALinkCheck(ctaLink) {
   return previousSibiling?.localName === 'h1' || twoPreviousSibiling?.localName === 'h1';
 }
 
+function findValidSibling(el, selectors) {
+  return [el.previousElementSibling, el.nextElementSibling].find(
+    (sibling) => sibling?.matches(selectors) && sibling.textContent.trim().length > 0,
+  );
+}
+
 function buildHeroBlock(main) {
   // don't create a hero if the first item is a block.
   const firstSection = main.querySelector('div');
@@ -61,12 +67,12 @@ function buildHeroBlock(main) {
     const headings = document.createElement('div');
     headings.className = 'hero-headings';
     const elems = [picture, headings];
-    if (h1.nextElementSibling && (h1.nextElementSibling.matches('h2,h3,h4')
-      // also consider a <p> without any children as sub heading
-      || (h1.nextElementSibling.matches('p') && !h1.nextElementSibling.children.length))) {
+
+    const sibling = findValidSibling(h1, 'h2,h3,h4') || findValidSibling(h1, 'p');
+    if (sibling) {
       const h4 = document.createElement('h4');
-      h4.innerHTML = h1.nextElementSibling.innerHTML;
-      h1.nextElementSibling.remove();
+      h4.innerHTML = sibling.innerHTML;
+      sibling.remove();
       headings.appendChild(h4);
     }
     headings.appendChild(h1);
