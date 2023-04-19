cc3ddcc9cfbfb75defa7851f3e0d822fd9be0c8d
Aravindo Wingeier <wingeier@adobe.com>
2023-04-18 18:42:09 -0700

    refactor download-content-markdown.mjs

--- tools/content-mirror/download-content-markdown.mjs
+++ tools/content-mirror/download-content-markdown.mjs
@@ -8,14 +8,27 @@ import path from "path";
 
 const DOMAIN = "https://main--vg-volvotrucks-us--hlxsites.hlx.page";
 
-async function createFolder(url) {
-  const folderPath = `./mirror${new URL(url).pathname.replace('/.*$', '')
-    .replace('/index.md', '')}`;
+function getFolderPathAndFilename(url) {
+  const prefix = `./hlxsite`;
+
+  const pathname = new URL(url).pathname;
+  // remove trailing slash
+  const withoutTrailingSlash = pathname.replace(/\/$/, '');
+
+  const folderName = withoutTrailingSlash.substring(0, withoutTrailingSlash.lastIndexOf('/'));
+  const folderPath = `${prefix}${folderName}`;
+
+  const filename = path.basename(withoutTrailingSlash);
+
+  return [folderPath, filename];
+}
+
+async function createFolder(folderPath) {
   try {
     await fs.promises.mkdir(folderPath, {recursive: true});
   } catch (ignore) {
+    console.log('failed to create folder: ', folderPath);
   }
-  return folderPath;
 }
 
 function mdUrl(url) {
@@ -26,7 +39,6 @@ function mdUrl(url) {
   }
 }
 
-
 async function getStatusUrls() {
   const response = await fetch("https://main--vg-volvotrucks-us--hlxsites.hlx.page/drafts/import-status.json");
   const json = await response.json();
@@ -36,7 +48,7 @@ async function getStatusUrls() {
 
 
 async function getUrls() {
-  const response = await fetch(DOMAIN + "/query-index.json");
+  const response = await fetch("https://main--vg-volvotrucks-us--hlxsites.hlx.page/query-index.json");
   const json = await response.json();
   return json.data.map(row => DOMAIN + row.path + "/")
     // remove duplicate slashes (usually at the end)
@@ -45,29 +57,39 @@ async function getUrls() {
 
 async function loadUrl(url) {
   try {
+    const [folderPath, filename] = getFolderPathAndFilename(url);
+
     const response = await fetch(url, {redirect: 'error'});
     if (response.ok) {
-      const folderPath = await createFolder(url);
-      await response.body.pipe(fs.createWriteStream(`${folderPath}/${path.basename(url)}`));
+      await createFolder(folderPath);
+      await response.body.pipe(fs.createWriteStream(`${folderPath}/${filename}`));
       console.log('ok: ', url);
     } else {
       if (url.includes("/index.md") && response.status === 404) {
         // retry without "/index"
         return loadUrl(url.replace("/index.md", ".md"));
       } else {
-        console.log('failed: ', url);
+        console.log('failed: ', url, response.status);
       }
     }
-  }catch (e) {
-    console.log('failed (maybe redirect): ', url);
+  } catch (e) {
+    if (e.message.includes("uri requested responds with a redirect")) {
+      console.log('failed (redirect): ', url);
+      return;
+    }
+    console.log('failed: ', url, e.message);
   }
 }
 
 async function main() {
-  const urls = [...await getUrls(), ...await getStatusUrls()];
-  await Promise.all(urls.map(async (url) => {
-    const adjustedUrl = mdUrl(url);
-    await loadUrl(adjustedUrl);
+  const urls = [...await getUrls(), ...await getStatusUrls()]
+  const unique = [...new Set(urls)];
+
+  // unique.forEach(url => console.log(url));
+  // return;
+
+  await Promise.all(unique.map(async (url) => {
+    await loadUrl(mdUrl(url));
   }));
 }
 
