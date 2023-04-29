8ab5c5265bbba8ae0479f488ecbce31513f33f29
Aravindo Wingeier <wingeier@adobe.com>
2023-04-29 11:24:53 -0700

    comperator without image

--- .github/visual-tests/tests/compare.spec.ts
+++ .github/visual-tests/tests/compare.spec.ts
@@ -1,28 +1,23 @@
 import {expect, test} from '@playwright/test';
+import {getComparator} from 'playwright-core/lib/utils';
 
 for (const path of ["/", "/trucks/"]) {
-  test(`compare ${path}`, async ({page}) => {
+  test(`compare ${path}`, async ({page}, testInfo) => {
 
     await page.goto(`https://${process.env.DOMAIN1}${path}`);
     await page.waitForTimeout(2000);
-
-    try {
-      await expect(page).toHaveScreenshot("screenshot.png");
-    } catch (error) {
-      console.log("ignoring", error.message);
-    }
-
-    try {
-      expect(await page.textContent('body')).toMatchSnapshot('body.txt');
-    } catch (error) {
-      console.log("ignoring", error.message);
-    }
-// TODO: instead create two screenshots and then compare. If they are different, then fail the test.
+    const beforeImage = await page.screenshot({
+      path: `./${testInfo.title}-before.png`
+    });
 
     await page.goto(`https://${process.env.DOMAIN2}/${path}`);
     await page.waitForTimeout(2000);
-    await expect(page).toHaveScreenshot("screenshot.png", {maxDiffPixels: 100});
-    expect(await page.textContent('body')).toMatchSnapshot('body.txt');
+    const afterImage = await page.screenshot({
+      path: `./${testInfo.title}-after.png`
+    });
+
+    const comparator = getComparator('image/png');
+    expect(comparator(beforeImage, afterImage)).toBeNull();
   });
 
 }
