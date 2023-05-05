308bc63c82f2b96e14c67361411851476feffcb2
Aravindo Wingeier <wingeier@adobe.com>
2023-05-05 16:34:10 -0700

    run npx

--- .github/visual-tests/run.bash
+++ .github/visual-tests/run.bash
@@ -4,7 +4,7 @@ set -e
 # TODO: get dynamically from config
 export DOMAIN1='main--vg-volvotrucks-us--hlxsites.hlx.live'
 export DOMAIN2='${GITHUB_HEAD_REF}--vg-volvotrucks-us--hlxsites.hlx.live'
-#npx playwright test | tee playwright.log
+npx playwright test | tee playwright.log
 
 if grep -q "$DOMAIN2" playwright.log; then
   echo "Diffs found"
