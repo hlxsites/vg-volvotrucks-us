c29f81f52eade0ecf757e360a572416b06eb864b
Aravindo Wingeier <wingeier@adobe.com>
2023-05-05 14:02:37 -0700

    simple report

--- .github/visual-tests/playwright.config.ts
+++ .github/visual-tests/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -38,7 +38,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
   },
 
   /* Configure projects for major browsers */
--- .github/visual-tests/tests/compare.spec.ts
+++ .github/visual-tests/tests/compare.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test, TestInfo} from '@playwright/test';
+import {test, TestInfo} from '@playwright/test';
 import {getComparator} from 'playwright-core/lib/utils';
 import {writeFile} from 'fs/promises';
 
@@ -16,7 +16,8 @@ for (const path of ["/", "/trucks/"]) {
       path: getScreenshotPath(testInfo, 'main')
     });
 
-    await page.goto(`https://${process.env.DOMAIN2}${path}`);
+    const url2 = `https://${process.env.DOMAIN2}${path}`;
+    await page.goto(url2);
     await page.waitForTimeout(2000);
     const afterImage = await page.screenshot({
       path: getScreenshotPath(testInfo, 'branch')
@@ -24,18 +25,23 @@ for (const path of ["/", "/trucks/"]) {
 
     const comparator = getComparator('image/png');
     const comparatorOptions = {
-          // maxDiffPixels: ,
-          maxDiffPixelRatio: 0.01,
-          // threshold: 0.99,
+      // maxDiffPixels: ,
+      maxDiffPixelRatio: 0.01,
+      // threshold: 0.99,
     }
     const result = comparator(beforeImage, afterImage, comparatorOptions);
-    if(result) {
+    if (result) {
       // store the diff image
       await writeFile(getScreenshotPath(testInfo, 'diff'), result.diff);
-      testInfo.attachments.push({ name: getScreenshotPath(testInfo, 'diff'), contentType: `image/png`, path: getScreenshotPath(testInfo, 'diff') });
-      console.log(`diff: ${path}: [diff](${getScreenshotPath(testInfo, 'diff')})`);
-      // expect(result.errorMessage, `diff: ${path}`).toBeNull();
+      testInfo.attachments.push({
+        name: getScreenshotPath(testInfo, 'diff'),
+        contentType: `image/png`,
+        path: getScreenshotPath(testInfo, 'diff')
+      });
+      if (result.errorMessage) {
+        console.log(`- ${url2} ${result.errorMessage}`);
+      }
     }
-  });
+  })
 
 }
