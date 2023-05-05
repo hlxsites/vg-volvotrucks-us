b02487f9e42393a1d4db8535cb38f8ca407ea24a
Aravindo Wingeier <wingeier@adobe.com>
2023-05-05 16:28:57 -0700

    use bash script

--- .github/visual-tests/run.bash
+++ .github/visual-tests/run.bash
@@ -1,31 +1,25 @@
 #!/usr/bin/env bash
-GITHUB_ENV=env.txt
-GITHUB_STEP_SUMMARY=summary.txt
-github.head_ref=ui-tests
-#set -x
+set -e
 
 # TODO: get dynamically from config
 export DOMAIN1='main--vg-volvotrucks-us--hlxsites.hlx.live'
-export DOMAIN2='${{ github.head_ref }}--vg-volvotrucks-us--hlxsites.hlx.live'
+export DOMAIN2='${GITHUB_HEAD_REF}--vg-volvotrucks-us--hlxsites.hlx.live'
 #npx playwright test | tee playwright.log
 
-if grep "$DOMAIN2" playwright.log; then
+if grep -q "$DOMAIN2" playwright.log; then
   echo "Diffs found"
   SUMMARY="### :small_orange_diamond: Visual differences detected: :zap:
   $(grep "$DOMAIN2" playwright.log)
 
-  See also diff image [in attached screenshots](${ GITHUB_SERVER_URL }/${ GITHUB_REPOSITORY }/actions/runs/${ GITHUB_RUN_ID })
+  See also diff image [in attached screenshots](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID})
   "
-
-  echo $SUMMARY
-  exit 1
 
   echo "$SUMMARY" >> "$GITHUB_STEP_SUMMARY"
 
   # using multi-line-vars from  https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-of-a-multiline-string
   EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
   echo "SUMMARY<<$EOF" >> "$GITHUB_ENV"
-  echo "$SUMMARY" "$GITHUB_ENV"
+  echo "$SUMMARY" >> "$GITHUB_ENV"
   echo "$EOF" >> "$GITHUB_ENV"
 else
   echo "No diffs found"
