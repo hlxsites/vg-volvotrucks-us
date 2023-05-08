a20460e751827a3927508438d6e6c337caad8b08
Aravindo Wingeier <wingeier@adobe.com>
2023-05-08 16:36:20 -0700

    add macktruck example

--- .github/visual-tests/run-locally.bash
+++ .github/visual-tests/run-locally.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#export DOMAIN_MAIN="main--vg-macktrucks-com--hlxsites.hlx.page"
+#export DOMAIN_BRANCH="update-from-boilerplate--vg-macktrucks-com--hlxsites.hlx.page"
+
 export DOMAIN_MAIN="main--vg-volvotrucks-us--hlxsites.hlx.live"
 export DOMAIN_BRANCH="426-service-contracts--vg-volvotrucks-us--hlxsites.hlx.live"
 
