3cd5e9776f75f194f8b7858b09b6f195420e1b59
Laura Jove <laura.jove@netcentric.biz>
2023-07-31 15:04:16 +0200

    New block images grid

---
+++ blocks/images-grid/images-grid.js
@@ -0,0 +1,52 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { removeEmptyTags } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  // all items are inside a ul list with classname called 'image-grid__items'
+  const ul = document.createElement('ul');
+  ul.classList.add('image-grid__items');
+
+  [...block.querySelectorAll(':scope > div > div')].forEach((cell) => {
+    // If cell contain any element, we add them in the ul
+    if (cell.childElementCount) {
+      const li = document.createElement('li');
+      li.classList.add('image-grid__item', 'border');
+      li.append(...cell.childNodes);
+      ul.append(li);
+    }
+    cell.remove();
+  });
+
+  block.firstElementChild.append(ul);
+
+  // give format to the list items
+  [...ul.children].forEach((li) => {
+    const section = document.createElement('div');
+
+    // add link to the image and move it outside of the wrapper
+    const title = li.querySelector('h3');
+    title.classList.add('image-grid__title');
+    let picture = li.querySelector('picture');
+
+    if (picture) {
+      const img = picture.lastElementChild;
+      // no width provided because we are using object-fit, we need the biggest option
+      const newPicture = createOptimizedPicture(img.src, img.alt, false);
+      picture.replaceWith(newPicture);
+      picture = newPicture;
+    }
+
+    section.prepend(picture);
+
+    // Add wrapper around the text content
+    const container = document.createElement('div');
+    container.className = 'image-grid__itemText';
+    container.innerHTML = li.innerHTML;
+    li.innerHTML = '';
+    section.append(container);
+    li.append(section);
+  });
+
+  // remove empty tags
+  removeEmptyTags(block);
+}
