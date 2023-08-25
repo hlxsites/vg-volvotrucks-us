ae06c39547b80de770c6700814ca2ee6f86ac059
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2023-08-25 11:56:25 +0200

    change createelement function

--- blocks/v2-feature-carousel/v2-feature-carousel.js
+++ blocks/v2-feature-carousel/v2-feature-carousel.js
@@ -1,4 +1,4 @@
-import { createElement, removeEmptyTags } from '../../scripts/scripts.js';
+import { createElement, removeEmptyTags } from '../../scripts/common.js';
 
 const blockname = 'v2-feature-carousel';
 
@@ -10,7 +10,7 @@ export default function decorate(block) {
   [...cardsWraper.querySelectorAll('p')].forEach((el) => {
     el.classList.add(`${blockname}__card`);
     const text = el.lastChild;
-    const textwrapper = createElement('span', `${blockname}__text`);
+    const textwrapper = createElement('span', { classes: `${blockname}__text` });
     textwrapper.textContent = text.textContent;
     el.append(textwrapper);
     text.remove();
