88fe2e47dd21abc4cd1d3b43fa931c2ef8bd5415
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2023-08-28 15:56:36 +0200

    resolve comments

--- blocks/v2-feature-carousel/v2-feature-carousel.js
+++ blocks/v2-feature-carousel/v2-feature-carousel.js
@@ -3,11 +3,12 @@ import { createElement, removeEmptyTags } from '../../scripts/common.js';
 const blockname = 'v2-feature-carousel';
 
 export default function decorate(block) {
-  const cardsWraper = block.firstElementChild;
+  const cardsWraper = block.querySelector('ul');
   cardsWraper.classList.add(`${blockname}__cards-list`);
-  cardsWraper.append(...cardsWraper.firstElementChild.children);
+  block.innerHTML = '';
+  block.append(cardsWraper);
 
-  [...cardsWraper.querySelectorAll('p')].forEach((el) => {
+  [...cardsWraper.querySelectorAll('li')].forEach((el) => {
     el.classList.add(`${blockname}__card`);
     const text = el.lastChild;
     const textwrapper = createElement('span', { classes: `${blockname}__text` });
