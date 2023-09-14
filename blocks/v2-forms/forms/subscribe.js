5083221cc54ba91d2393f7959c65509d5accbfdc
Laura Jové <laura.jove@netcentric.biz>
2023-09-14 17:05:10 +0200

    newsletter first draft

---
+++ blocks/v2-forms/forms/subscribe.js
@@ -0,0 +1,12 @@
+const formContent = `
+  <input
+    type="text"
+    class="elq-item-input"
+    name="firstName"
+    value=""
+    style="width: 100%"
+  />
+  <button type="submit">Send</button>
+`;
+
+export default formContent;
---
+++ blocks/v2-forms/v2-forms.js
@@ -0,0 +1,51 @@
+import { createElement } from '../../scripts/common.js';
+
+// cache contains the form element that should be reused
+const eloquaFormCache = new Map();
+
+const addForm = async (block) => {
+  const displayValue = block.style.display;
+  block.style.display = 'none';
+
+  const formName = block.firstElementChild.innerText.trim();
+  const formAction = block.firstElementChild.nextElementSibling.innerText.trim();
+
+  const formContent = await import(`./forms/${formName}.js`);
+
+  const form = `
+    <form
+      method="post"
+      name="request-quote"
+      action="${formAction}"
+      class="elq-form"
+    >${formContent.default}
+    </form>
+  `;
+
+  if (eloquaFormCache.get(formName)) {
+    const cachedForm = eloquaFormCache.get(formName);
+    block.append(cachedForm);
+    block.style.display = displayValue;
+    return;
+  }
+
+  const formWrapper = createElement('div', { classes: 'eloqua-form-container' });
+  formWrapper.innerHTML = form;
+  block.innerHTML = '';
+  block.append(formWrapper);
+  eloquaFormCache.set(formName, formWrapper);
+
+  block.style.display = displayValue;
+};
+
+export default async function decorate(block) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some((e) => e.isIntersecting)) {
+      observer.disconnect();
+      addForm(block);
+    }
+  }, {
+    rootMargin: '300px',
+  });
+  observer.observe(block);
+}
