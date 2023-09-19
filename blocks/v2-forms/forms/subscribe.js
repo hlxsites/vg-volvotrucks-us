992436664f1c78922f8ff5f2e16fd81247000715
Laura Jové <laura.jove@netcentric.biz>
2023-09-19 09:17:10 +0200

    fix label + input relation

--- blocks/v2-forms/forms/subscribe.js
+++ blocks/v2-forms/forms/subscribe.js
@@ -1,7 +1,7 @@
 const formContent = `
   <div class="v2-forms__floating-label-group">
-    <input type="email" name="email" autocomplete="off" placeholder=" " required />
-    <label for="email" class="v2-forms__floating-label">Enter your email</label>
+    <input type="email" id="subscribe-email" name="email" autocomplete="off" placeholder=" " required />
+    <label for="subscribe-email" class="v2-forms__floating-label">Enter your email</label>
   </div>
   <button class="button primary" type="submit">Submit</button>
 `;
