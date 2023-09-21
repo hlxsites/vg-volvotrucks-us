1e92db0201042025f88111482312e75d3e5561cf
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2023-09-21 14:07:41 +0200

    #75 Refactor

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -169,8 +169,6 @@ const optimiseImage = (picture) => {
 };
 
 const buildMenuContent = (menuData, navEl, menuFooter) => {
-  menuData.querySelectorAll('picture').forEach(optimiseImage);
-
   const menus = [...menuData.querySelectorAll('.menu')];
   const navLinks = [...navEl.querySelectorAll(`.${blockClass}__main-nav-link`)];
 
@@ -274,6 +272,9 @@ export default async function decorate(block) {
     menuContent,
     menuContentFooter,
   ] = content.children;
+
+  menuContent.querySelectorAll('picture').forEach(optimiseImage);
+
   const nav = createElement('nav', { classes: [`${blockClass}__nav`] });
   const navContent = document.createRange().createContextualFragment(`
     <div class="${blockClass}__menu-overlay"></div>
