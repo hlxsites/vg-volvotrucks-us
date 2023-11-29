072db80d405a1802b48613994d4f6ade0deb5f54
taimurCognizant <150666850+taimurCognizant@users.noreply.github.com>
2023-11-29 17:49:11 +0100

    Countdown in hero #66 (#264)
    * add countdown timer in hero block
    * refactor gaps, UI improvements for all variants
    * fix button padding for tablet
    * better text wrapping
    * fix hero container takes at least the height of the viewport, and in case of overflow content, the height expands
    * update hero gradient
    * add variant to right align background in hero
    * refactor better class names for positioning
    * fix hero background
    * add play icon
    
    ---------
    
    Co-authored-by: Syb Wartna <sybren.wartna@cognizant.com>

--- blocks/v2-hero/v2-hero.js
+++ blocks/v2-hero/v2-hero.js
@@ -3,16 +3,49 @@ import {
   createVideo,
 } from '../../scripts/video-helper.js';
 import {
-  createElement,
-  removeEmptyTags,
-  variantsClassesToBEM,
+  createElement, getTextLabel, removeEmptyTags, variantsClassesToBEM,
 } from '../../scripts/common.js';
 
-const variantClasses = ['centered', 'left', 'bottom', 'dark'];
+const variantClasses = ['centered', 'left-center', 'center-bottom', 'dark', 'background-right'];
+let intervalId = null;
+const blockName = 'v2-hero';
+
+function updateCountdownElement(block, elementId, value, label) {
+  const element = block.querySelector(`#${elementId}`);
+  element.textContent = value.toString().padStart(2, '0');
+  element.nextElementSibling.textContent = label;
+}
+
+function updateCountdown(eventTime, block) {
+  const now = new Date();
+  const diff = eventTime - now;
+
+  // Check if the event time has passed
+  if (diff <= 0) {
+    block.querySelector(`.${blockName}__countdown-wrapper`)?.remove();
+    clearInterval(intervalId);
+    return;
+  }
+
+  // Calculate time left
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // Format labels
+  const dayLabel = days === 1 ? getTextLabel('day') : `${getTextLabel('day')}s`;
+  const hourLabel = hours === 1 ? getTextLabel('hour') : `${getTextLabel('hour')}s`;
+  const minuteLabel = minutes === 1 ? getTextLabel('minute') : `${getTextLabel('minute')}s`;
+  const secondLabel = seconds === 1 ? getTextLabel('second') : `${getTextLabel('second')}s`;
+
+  updateCountdownElement(block, 'days', days, dayLabel);
+  updateCountdownElement(block, 'hours', hours, hourLabel);
+  updateCountdownElement(block, 'minutes', minutes, minuteLabel);
+  updateCountdownElement(block, 'seconds', seconds, secondLabel);
+}
 
 export default async function decorate(block) {
-  const blockName = 'v2-hero';
-
   // add Hero variant classnames
   variantsClassesToBEM(block.classList, variantClasses, blockName);
 
@@ -45,6 +78,38 @@ export default async function decorate(block) {
   const content = block.querySelector(':scope > div > div');
   content.classList.add(`${blockName}__content`);
 
+  // Countdown timer
+  const blockSection = block.parentElement?.parentElement;
+  if (blockSection && blockSection.dataset?.countdownDate) {
+    const countDownWrapper = createElement('div', { classes: `${blockName}__countdown-wrapper` });
+    countDownWrapper.innerHTML = `<div class="${blockName}__countdown">
+  <div class="${blockName}__countdown-segment">
+    <div id="days" class="${blockName}__countdown-number">00</div>
+    <div class="${blockName}__countdown-label">Days</div>
+  </div>
+  <div class="${blockName}__countdown-segment">
+    <div id="hours" class="${blockName}__countdown-number">00</div>
+    <div class="${blockName}__countdown-label">Hours</div>
+  </div>
+  <div class="${blockName}__countdown-segment">
+    <div id="minutes" class="${blockName}__countdown-number">00</div>
+    <div class="${blockName}__countdown-label">Minutes</div>
+  </div>
+  <div class="${blockName}__countdown-segment">
+    <div id="seconds" class="${blockName}__countdown-number">00</div>
+    <div class="${blockName}__countdown-label">Seconds</div>
+  </div>
+</div>`;
+    content.prepend(countDownWrapper);
+
+    const eventTimeIso = blockSection.dataset.countdownDate;
+    const eventTime = new Date(eventTimeIso);
+    updateCountdown(eventTime, block);
+    intervalId = setInterval(() => {
+      updateCountdown(eventTime, block);
+    }, 1000);
+  }
+
   // convert all headings to h1
   const headings = [...content.querySelectorAll('h1, h2, h3, h4, h5, h6')];
   headings.forEach((heading) => {
@@ -57,6 +122,10 @@ export default async function decorate(block) {
       heading.classList.add(`${blockName}__title`);
     }
   });
+
+  // render all paragraph as H6 with the class
+  const paragraphs = [...content.querySelectorAll('p')];
+  paragraphs.forEach((paragraph) => paragraph.classList.add('h6'));
 
   const buttonsWrapper = createElement('div', { classes: `${blockName}__buttons-wrapper` });
   const ctaButtons = content.querySelectorAll('.button-container > a');
