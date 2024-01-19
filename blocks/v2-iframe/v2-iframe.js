11f39ce8e78534d3e65ff35709e48669853c217e
Syb Wartna <sybren.wartna@cognizant.com>
2024-01-19 14:17:14 +0100

    pass parameters to truck builder

--- blocks/v2-iframe/v2-iframe.js
+++ blocks/v2-iframe/v2-iframe.js
@@ -1,7 +1,8 @@
 import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
 export default async function decorate(block) {
-  const link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
+  let link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
+  link += window.location.search ? window.location.search : '';
   const title = block.querySelector('h1, h2, h3, h4, h5, h6').textContent;
   const iframe = createElement('iframe', { props: { src: link, frameborder: 0, title } });
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
