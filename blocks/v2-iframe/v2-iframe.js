ab25e7d9bfcf0ab7897cdcf1a004457a48024254
Syb Wartna <sybren.wartna@cognizant.com>
2024-01-22 20:34:49 +0100

    add redundancy to URLs

--- blocks/v2-iframe/v2-iframe.js
+++ blocks/v2-iframe/v2-iframe.js
@@ -2,9 +2,24 @@ import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
 export default async function decorate(block) {
   let link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
-  link += window.location.search ? `#/summary/${window.location.search}` : '';
+
+  function removeDuplicateParams(queryString) {
+    const params = new URLSearchParams(queryString);
+    const uniqueParams = new URLSearchParams();
+  
+    for (const [key, value] of params) {
+      uniqueParams.set(key, value);
+    }
+  
+    return uniqueParams.toString();
+  }
+  
+  link += window.location.search ? `#/summary/?${removeDuplicateParams(window.location.search)}` : '';
+  if (!window.location.search && window.location.hash) {
+    link += window.location.hash;
+  }
   const title = block.querySelector('h1, h2, h3, h4, h5, h6').textContent;
-  const iframe = createElement('iframe', { props: { src: link, frameborder: 0, title } });
+  const iframe = createElement('iframe', { props: { src: link, frameborder: 0, title, allow: 'clipboard-write' } });
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
 
   variantsClassesToBEM(block.classList, ['full-viewport'], 'v2-iframe');
