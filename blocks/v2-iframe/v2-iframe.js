732f8284bfc6122c918cf47772ee922b0c401f0b
Syb Wartna <sybren.wartna@cognizant.com>
2024-01-22 18:08:47 +0100

    update link of truck configurator

--- blocks/v2-iframe/v2-iframe.js
+++ blocks/v2-iframe/v2-iframe.js
@@ -2,7 +2,7 @@ import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
 export default async function decorate(block) {
   let link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
-  link += window.location.search ? window.location.search : '';
+  link += window.location.search ? `#/summary/${window.location.search}` : '';
   const title = block.querySelector('h1, h2, h3, h4, h5, h6').textContent;
   const iframe = createElement('iframe', { props: { src: link, frameborder: 0, title } });
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
