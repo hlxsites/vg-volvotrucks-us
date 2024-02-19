554fe892206c5bedd1164f5e4236470dccc26d90
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2024-02-19 16:30:54 +0100

    RENAMING

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,13 +1,22 @@
+// ONE TRUST COOKIE CONSENT
+export const DATA_DOMAIN_SCRIPT = 'e8ffed56-4bb1-43fd-9b41-bc4385267ec8';
+
+// GOOGLE TAG MANAGER ID
+export const GTM_ID = 'GTM-KP9KZWR';
+
+// HOTJAR ID
+export const HOTJAR_ID = 1139895;
+
 // COOKIE Constants
-export const COOKIES = {
+export const COOKIE_VALUES = {
   performance: 'C0002:1',
   social: 'C0005:1',
   targeting: 'C0004:1',
 };
 
 // ACCOUNT ENGAGEMENT TRACKING Constants
-export const ACCOUNT_ENGAGEMENT_TRACKING = {
-  piAId: '1039333',
-  piCId: '25825',
+export const ACCOUNT_ENGAGEMENT_TRACKING_CONSTANTS = {
+  piAId: 1039333,
+  piCId: 25825,
   piHostname: 'pi.pardot.com',
 };
