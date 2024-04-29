d102dfa76a44dab23912d0d47a5e606a6dcde83f
Syb Wartna <sybren.wartna@cognizant.com>
2024-04-29 17:44:18 +0200

    simplify code

--- blocks/v2-embed/v2-embed.js
+++ blocks/v2-embed/v2-embed.js
@@ -32,33 +32,29 @@ const extractAspectRatio = (block) => {
   const aspectRatioClass = Array.from(block.classList)
     .find((className) => aspectRatioRegex.test(className));
 
-  if (!aspectRatioClass) {
-    return null;
-  }
+  const match = aspectRatioClass?.match(aspectRatioRegex);
 
-  const match = aspectRatioClass.match(aspectRatioRegex);
-  if (match) {
-    return {
-      width: parseInt(match[1], 10),
-      height: parseInt(match[2], 10),
-    };
-  }
-
-  return null;
+  return match ? {
+    width: parseInt(match[1], 10),
+    height: parseInt(match[2], 10),
+  } : null;
 };
 
-const retrieveVideoConfig = (block, aspectRatio) => ({
-  ...(aspectRatio ? { aspectRatio: `${aspectRatio.width}:${aspectRatio.height}` } : {}),
-  ...(block.querySelector('img')?.getAttribute('src')
-    ? { poster: new URL(block.querySelector('img').getAttribute('src'), window.location.href).href }
-    : {}),
-  autoplay: block.classList.contains('autoplay') ? 'any' : false,
-  muted: block.classList.contains('autoplay'),
-  loop: block.classList.contains('loop'),
-  controls: !block.classList.contains('disable-controls'),
-  disablePictureInPicture: block.classList.contains('disable-picture-in-picture'),
-  language: document.documentElement.lang,
-});
+const retrieveVideoConfig = (block, aspectRatio) => {
+  const image = block.querySelector('img');
+  const poster = image ? new URL(image.getAttribute('src'), window.location.href).href : undefined;
+
+  return {
+    ...(aspectRatio ? { aspectRatio: `${aspectRatio.width}:${aspectRatio.height}` } : {}),
+    ...(poster ? { poster } : {}),
+    autoplay: block.classList.contains('autoplay') ? 'any' : false,
+    muted: block.classList.contains('autoplay'),
+    loop: block.classList.contains('loop'),
+    controls: !block.classList.contains('disable-controls'),
+    disablePictureInPicture: block.classList.contains('disable-picture-in-picture'),
+    language: document.documentElement.lang,
+  };
+};
 
 const configureVideo = (block, videoId) => {
   const config = retrieveVideoConfig(block);
