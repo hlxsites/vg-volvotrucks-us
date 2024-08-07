dca43f0096da07f9892a6fb1259d018b7dd20658
Alexis Coelho <alexis.coelho@netcentric.biz>
2024-08-07 09:04:01 +0200

    Add generate news feed failsafe code #691

--- .github/ci/generate-news-feed.js
+++ .github/ci/generate-news-feed.js
@@ -8,42 +8,59 @@ const targetFile = `${process.env.NEWS_TARGET_DIRECTORY}/feed.xml`;
 const limit = 1000;
 
 async function main() {
-  const allPosts = await fetchBlogPosts();
-  console.log(`found ${allPosts.length} posts`);
+  let allPosts;
 
-  const feedMetadata = await fetchBlogMetadata();
-
-  const newestPost = allPosts
-    .map((post) => new Date(post.publishDate * 1000))
-    .reduce((maxDate, date) => (date > maxDate ? date : maxDate), new Date(0));
-
-  const feed = new Feed({
-    title: feedMetadata.title,
-    description: feedMetadata.description,
-    id: feedMetadata.link,
-    link: feedMetadata.link,
-    updated: newestPost,
-    generator: 'AEM News feed generator (GitHub action)',
-    language: feedMetadata.lang,
-  });
-
-  allPosts.forEach((post) => {
-    const link = feedMetadata["site-root"] + post.path;
-    feed.addItem({
-      title: post.title,
-      id: link,
-      link,
-      content: post.description,
-      date: new Date(post.publishDate * 1000),
-      published: new Date(post.publishDate * 1000),
-    });
-  });
-
-  if (!fs.existsSync(targetDirectory)) {
-    fs.mkdirSync(targetDirectory);
+  try {
+    allPosts = await fetchBlogPosts();
+  } catch (error) {
+    console.error('Error fetching Blog posts');
   }
-  fs.writeFileSync(targetFile, feed.atom1());
-  console.log('wrote file to ', targetFile);
+
+  if (allPosts) {
+    console.log(`found ${allPosts.length} posts`);
+
+    let feedMetadata;
+  
+    try {
+      feedMetadata = await fetchBlogMetadata();
+    } catch (error) {
+      console.error('Error fetching Blog posts metadata');
+    }
+
+    if (feedMetadata) {
+      const newestPost = allPosts
+        .map((post) => new Date(post.publishDate * 1000))
+        .reduce((maxDate, date) => (date > maxDate ? date : maxDate), new Date(0));
+    
+      const feed = new Feed({
+        title: feedMetadata.title,
+        description: feedMetadata.description,
+        id: feedMetadata.link,
+        link: feedMetadata.link,
+        updated: newestPost,
+        generator: 'AEM News feed generator (GitHub action)',
+        language: feedMetadata.lang,
+      });
+    
+      allPosts.forEach((post) => {
+        const link = feedMetadata["site-root"] + post.path;
+        feed.addItem({
+          title: post.title,
+          id: link,
+          link,
+          content: post.description,
+          date: new Date(post.publishDate * 1000),
+          published: new Date(post.publishDate * 1000),
+        });
+      });
+    
+      if (!fs.existsSync(targetDirectory)) {
+        fs.mkdirSync(targetDirectory);
+      }
+      fs.writeFileSync(targetFile, feed.atom1());
+      console.log('wrote file to ', targetFile);
+    }
+  }
 }
 
 async function fetchBlogPosts() {
